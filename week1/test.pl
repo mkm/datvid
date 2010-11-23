7c2539c357c843d89500a0bd52a27a305abd02be
mkm <kragh@diku.dk>
2010-11-23 15:44:56 +0100

    Improved test script.

--- week1/test.pl
+++ week1/test.pl
@@ -8,12 +8,13 @@ $destfile = "minmor";
 open (DATAFILE, '>testdata');
 open (AGGRFILE, '>testaggr');
 
-for ($size = 1; $size <= 100; $size++) {
+for ($size = 5; $size <= 120; $size += 5) {
+    print "Size: $size\n";
     print DATAFILE "Size: $size\n";
     $count = $size * 1024 * 1024 / 512;
     `dd if=/dev/urandom of=$srcfile count=$count`;
     $total = 0;
-    for ($i = 0; $i < 10; $i++) {
+    for ($i = 0; $i < 8; $i++) {
         $before = Time::HiRes::time();
         `cp $srcfile $destfile`;
         $after = Time::HiRes::time();
